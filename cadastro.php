f80cac45d35973af9bc44ee0e23ff0c8fbb513c4
Giovana Brito <britogiovana089@gmail.com>
2025-05-24 20:53:45 -0300

    melhorias no codigo de cadastro

--- cadastro.php
+++ cadastro.php
@@ -5,23 +5,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $senha = $_POST['senha'] ?? '';
 
     try {
-        // Conectar ao banco de dados
+    
         $pdo = new PDO('mysql:host=localhost;dbname=desenvolvimentoweb', 'root', '');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        // Hash da senha
+        
         $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
 
-        // Preparar e executar a consulta
+     
         $stmt = $pdo->prepare('INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)');
         $stmt->execute([$nome, $email, $senha_hash]);
 
-        // Redirecionar para a tela de login após o cadastro
         header('Location: login.html');
         exit();
 
     } catch (PDOException $e) {
-        // Exibir mensagem de erro, se houver
+        
         echo "Erro ao cadastrar usuário: " . $e->getMessage();
     }
 
